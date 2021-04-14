2ab8149d9d50b7b4e8e7f7ffea9069618db1f1e5
novaariyanto <bajangsampoerna@gmail.com>
2021-04-14 13:07:56 +0700

    master

--- send_text.php
+++ send_text.php
@@ -1,17 +1,21 @@
 <?php
 
 $curl = curl_init();
-$data =["id_device"=> "865", "message"=> "test","tujuan"=> "62895361034833@s.whatsapp.net"];
-$payload = json_encode($data );
+$device_api_key = "4YwLki3nduP8";
+$data = [
+    "message" => "test",
+    "tujuan" => "62895361034833@s.whatsapp.net",
+];
+$payload = json_encode($data);
 
-$ch = curl_init( "https://whatsva.com/api/sendText" );
+$ch = curl_init("https://whatsva.com/api/sendMessage");
 # Setup request to send json via POST.
 
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',"apikey: BhJFNeca"));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', "apikey: " . $device_api_key));
 # Return response instead of printing.
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 # Send request.
 $result = curl_exec($ch);
 curl_close($ch);
--- testsendfileurl.php
+++ testsendfileurl.php
@@ -1,14 +1,14 @@
 <?php
+    $curl = curl_init();
+    $alamat_gateway = "https://whatsva.com/api/sendFileUrl";
+    $token = "8XeimL7B";
+    $id_device = "206";
+    $tujuan = "62818438289@s.whatsapp.net";
+    $message = "bro ";
 
-$curl = curl_init();
-$alamat_gateway = "https://whatsva.com/api/sendFileUrl";
-$token = "8XeimL7B";
-$id_device = "206";
-$tujuan = "62895361034833@s.whatsapp.net";
-$message = "halo";
+
 $file_url = "http://www.untag-smd.ac.id/files/Perpustakaan_Digital_2/Membongka%20Gurita%20Cikeas.pdf";
 $url_opt = $alamat_gateway."?id_device=".$id_device."&tujuan=".$tujuan."&message=".$message."&file_url=".$file_url;
-
 curl_setopt_array($curl, array(
   CURLOPT_URL => $url_opt,
   CURLOPT_RETURNTRANSFER => true,
